12aa822660c7f67bb3f16be6b468d09d6f9492fc
Brettschneider Matthias (CC-DA/EAR1) <Matthias.Brettschneider@Bosch.com>
2016-06-16 12:38:55 +0200

    added the package configuration for this package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tippspiel',
+      version='0.1',
+      description='A simple class for tracking football tip games',
+      url='http://github.com/frosch03/tippspiel',
+      author='Matthias Brettschneider',
+      author_email='frosch03@frosch03.de',
+      license='BSD',
+      packages=['tippspiel'],
+      zip_safe=False)
