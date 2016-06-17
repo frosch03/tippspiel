0049754196c023a848113cbd59a016b10efca89a
Matthias Brettschneider <frosch03@gmail.com>
2016-06-18 00:39:43 +0200

    Updated packet description and added binary

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(name='tippspiel',
       version='0.1',
-      description='A simple class for tracking football tip games',
+      description='A simple app to track a football bet games',
       url='http://github.com/frosch03/tippspiel',
       author='Matthias Brettschneider',
       author_email='frosch03@frosch03.de',
       license='BSD',
       packages=['tippspiel'],
+      entry_points={
+          'console_scripts': ['tipps=tippspiel.command_line:main']
+      },
       zip_safe=False)
